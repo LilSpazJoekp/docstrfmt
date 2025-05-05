8d9d320ed35312ebf36c7a2c18693e9d4403357d
Andre Anjos <andre.dos.anjos@gmail.com>
2025-05-05 14:35:09 +0200

    docstrfmt: add pragma no cover on type-checking

--- docstrfmt/rst_extras.py
+++ docstrfmt/rst_extras.py
@@ -10,7 +10,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING, Any, TypeVar
 
-if TYPE_CHECKING:
+if TYPE_CHECKING:  # pragma: no cover
     from collections.abc import Iterator
 
 import docutils
