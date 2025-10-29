e49ac529def6caef7c34d9c9da038d8148fafd07
LilSpazJoekp <LilSpazJoekp@users.noreply.github.com>
2025-10-29 15:48:37 +0000

    Bump to v2.0.0

--- docstrfmt/__init__.py
+++ docstrfmt/__init__.py
@@ -4,4 +4,4 @@ from . import debug, docstrfmt, rst_extras, util
 from .const import DEFAULT_EXCLUDE, DEFAULT_LINE_LENGTH
 from .docstrfmt import Manager
 
-__version__ = "1.11.2.dev0"
+__version__ = "2.0.0"
