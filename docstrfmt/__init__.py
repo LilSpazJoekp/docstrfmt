706db9b03a458169438b2fbe8ccb95ba4ec6beca
LilSpazJoekp <LilSpazJoekp@users.noreply.github.com>
2025-10-29 15:48:37 +0000

    Set development version v2.0.1.dev0

--- docstrfmt/__init__.py
+++ docstrfmt/__init__.py
@@ -4,4 +4,4 @@ from . import debug, docstrfmt, rst_extras, util
 from .const import DEFAULT_EXCLUDE, DEFAULT_LINE_LENGTH
 from .docstrfmt import Manager
 
-__version__ = "2.0.0"
+__version__ = "2.0.1.dev0"
